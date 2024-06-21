3819c62d9be9b4c234cebc96ed94fd63fd198238
Nathandiv <k.n.hlungwani@gmail.com>
2024-06-21 10:01:52 +0200

    finished arrays with objects and methods

---
+++ main.js
@@ -0,0 +1,230 @@
+// let nested = [
+//     ['Aplles','Cherries','Banana','Oranges'],
+//     ['Carrot','Tomatoes','Onoin','Pumpkin'],
+//     ['Rama','Jam','Chess','Ham'],
+//     ['Milk','Juice','Colddrink','Coffee'],
+// ];
+ 
+// console.log(nested)
+
+// let personInfo = [
+//     {Name :'Nathan', surName :'Hlungwani',Age: 30, isMale :true,}];
+
+//     let Cellphone =[{Phone:'Honor', serialNumber :14377568, yearModel :2022,}]; 
+//     let fruist = [{fruitName:'Avocado', sugarLevel :10, cellular:100, Seasons:'Winter'}];
+
+// let Beauty =  [
+//     model1 = {
+//       name: "Liesl ",
+//       surName : "Laurie",
+//       Age: 34,
+//       Location: "South Africa",
+//       sayHello: function () {
+//         return ("Hello"+ " "+ model1.name + " " + model1.surName)
+//       },
+
+//       Welcome: function () {
+//         console.log("Hello to Miss South Africa")
+
+//       },
+     
+//       myJun: function () {
+//         console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.") 
+
+//       },
+
+//     },
+
+//    model2 = {
+//         name: "Ntandoyenkosi  ",
+//         surName : "Kunene",
+//         Age: 30,
+//         Location: "South Africa",
+//         sayHello: function () {
+//           return ("Hello"+ Beauty.name + " " + Beauty.surNameName)
+//         },
+  
+//         Welcome: function () {
+//           console.log("Hello to Miss South Africa")
+  
+//         },
+       
+//         myJun: function () {
+//           console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.") 
+  
+//         },
+  
+//       },
+
+//       model3 = {
+//         name: "  Demi-Leigh ",
+//         surName : "Nel-Peters",
+//         Age: 37,
+//         Location: "South Africa",
+//         sayHello: function () {
+//           return ("Hello"+ Beauty.name + " " + Beauty.surNameName)
+//         },
+  
+//         Welcome: function () {
+//           console.log("Hello to Miss South Africa")
+  
+//         },
+       
+//         myJun: function () {
+//           console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.") 
+  
+//         },
+  
+//       },
+
+//       model4 = {
+//         name: "  Adè  ",
+//         surName : "van Heerden",
+//         Age: 43,
+//         Location: "South Africa",
+
+//         sayHello: function () {
+//           return ("Hello "+ " "+ Beauty.name + " " + Beauty.surNameName)
+//         },
+  
+//         Welcome: function () {
+//           console.log("Hello to Miss South Africa")
+  
+//         },
+       
+//         myJun: function () {
+//           console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.") 
+  
+//         },
+  
+//       },
+     
+//   ];
+  
+//   console.log(model1.sayHello())
+//   console.log(model1.Welcome())
+//   console.log(model1.myJun())
+
+  // let cars =  [
+  //   car1 = {
+  //     Model: "E63",
+  //     Type : "Merceders AMG",
+  //     Year: 2023,
+  //     Km: 60000,
+  //     Origin: "Germany",
+  //     isHybraid: false,
+  //     mycar: function () {
+  //       return ("Hello your car is a"+ " "+ car1.Model+ " " + car1.Type)
+  //     },
+
+  //     Why: function () {
+  //       console.log("Its a" + "" +car1.Model + "A Beast in a Suit")
+
+  //     },
+     
+  //     myJun: function () {if (car1.Km >= 100000){
+  //       console.log(`Your car is stil breathing no need for Mechanical check ups `);
+  //     }
+  //     else {
+  //               console.log(`Take your car for mechanical checks and repairs `);
+
+  //     }
+
+  //     },
+
+  //   },
+
+  //   car2 = {
+  //     Model: "RS3",
+  //     Type : "Audi",
+  //     Year: 2023,
+  //     Km: 90000,
+  //     Origin: "Germany",
+  //     isHybraid: false,
+  //     mycar: function () {
+  //       return ("Hello your car is a"+ " "+ car2.Model+ " " +car2.Type)
+  //     },
+
+  //     Why: function () {
+  //       console.log("Its a" + "" +car2.Model + "A Beast in a Suit")
+
+  //     },
+     
+  //     myJun: function () {if (car2.Km >= 100000){
+  //       console.log(`Your car is stil breathing no need for Mechanical check ups `);
+  //     }
+  //     else {
+  //               console.log(`Take your car for mechanical checks and repairs `);
+
+  //     }
+
+  //     },
+
+  //   },
+  //   car3 = {
+  //     Model: "X5",
+  //     Type : "BMW",
+  //     Year: 2020,
+  //     Km: 110000,
+  //     Origin: "Germany",
+  //     isHybraid: true,
+  //     mycar: function () {
+  //       return ("Hello your car is a"+ " "+  car3.Model+ " " + car3.Type)
+  //     },
+
+  //     Why: function () {
+  //       console.log("Its a" + "" + car3.Model + "A Beast in a Suit")
+
+  //     },
+     
+  //     myJun: function () {if ( car3.Km >= 100000){
+  //       console.log(`Your car is stil breathing no need for Mechanical check ups `);
+  //     }
+  //     else {
+  //               console.log(`Take your car for mechanical checks and repairs `);
+
+  //     }
+
+  //     },
+
+  //   },
+  //   car4 = {
+  //     Model: "GTI 7,5",
+  //     Type : "WV",
+  //     Year: 2023,
+  //     Km: 180000,
+  //     Origin: "Germany",
+  //     isHybraid: false,
+  //     mycar: function () {
+  //       return ("Hello your car is a"+ " "+  car4.Model+ " " + car4.Type)
+  //     },
+
+  //     Why: function () {
+  //       console.log("Its a" + "" + car4.Model + "A Beast in a Suit")
+
+  //     },
+     
+  //     myJun: function () {if ( car4.Km >= 100000){
+  //       console.log(`Your car is stil breathing no need for Mechanical check ups `);
+  //     }
+  //     else {
+  //               console.log(`Take your car for mechanical checks and repairs `);
+
+  //     }
+
+  //     },
+
+  //   },
+     
+  // ];
+
+  // console.log(car4.mycar())
+  // console.log(car4.Why())
+  // console.log(car4.myJun())
+
+
+
+
+
+
+
