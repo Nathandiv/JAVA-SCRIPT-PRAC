6d29c935a0056e2d926c04ac395113ee09692279
Nathandiv <k.n.hlungwani@gmail.com>
2024-06-21 16:16:10 +0200

    did loops and its logic

--- main.js
+++ main.js
@@ -4,13 +4,13 @@
 //     ['Rama','Jam','Chess','Ham'],
 //     ['Milk','Juice','Colddrink','Coffee'],
 // ];
- 
+
 // console.log(nested)
 
 // let personInfo = [
 //     {Name :'Nathan', surName :'Hlungwani',Age: 30, isMale :true,}];
 
-//     let Cellphone =[{Phone:'Honor', serialNumber :14377568, yearModel :2022,}]; 
+//     let Cellphone =[{Phone:'Honor', serialNumber :14377568, yearModel :2022,}];
 //     let fruist = [{fruitName:'Avocado', sugarLevel :10, cellular:100, Seasons:'Winter'}];
 
 // let Beauty =  [
@@ -27,9 +27,9 @@
 //         console.log("Hello to Miss South Africa")
 
 //       },
-     
+
 //       myJun: function () {
-//         console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.") 
+//         console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.")
 
 //       },
 
@@ -43,17 +43,17 @@
 //         sayHello: function () {
 //           return ("Hello"+ Beauty.name + " " + Beauty.surNameName)
 //         },
-  
+
 //         Welcome: function () {
 //           console.log("Hello to Miss South Africa")
-  
+
 //         },
-       
+
 //         myJun: function () {
-//           console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.") 
-  
+//           console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.")
+
 //         },
-  
+
 //       },
 
 //       model3 = {
@@ -64,17 +64,17 @@
 //         sayHello: function () {
 //           return ("Hello"+ Beauty.name + " " + Beauty.surNameName)
 //         },
-  
+
 //         Welcome: function () {
 //           console.log("Hello to Miss South Africa")
-  
+
 //         },
-       
+
 //         myJun: function () {
-//           console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.") 
-  
+//           console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.")
+
 //         },
-  
+
 //       },
 
 //       model4 = {
@@ -86,160 +86,200 @@
 //         sayHello: function () {
 //           return ("Hello "+ " "+ Beauty.name + " " + Beauty.surNameName)
 //         },
-  
+
 //         Welcome: function () {
 //           console.log("Hello to Miss South Africa")
-  
+
 //         },
-       
+
 //         myJun: function () {
-//           console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.") 
-  
+//           console.log("Miss South Africa is a prestigious beauty pageant that has been held annually since 1956.")
+
 //         },
-  
+
 //       },
-     
+
 //   ];
-  
+
 //   console.log(model1.sayHello())
 //   console.log(model1.Welcome())
 //   console.log(model1.myJun())
 
-  // let cars =  [
-  //   car1 = {
-  //     Model: "E63",
-  //     Type : "Merceders AMG",
-  //     Year: 2023,
-  //     Km: 60000,
-  //     Origin: "Germany",
-  //     isHybraid: false,
-  //     mycar: function () {
-  //       return ("Hello your car is a"+ " "+ car1.Model+ " " + car1.Type)
-  //     },
+// let cars =  [
+//   car1 = {
+//     Model: "E63",
+//     Type : "Merceders AMG",
+//     Year: 2023,
+//     Km: 60000,
+//     Origin: "Germany",
+//     isHybraid: false,
+//     mycar: function () {
+//       return ("Hello your car is a"+ " "+ car1.Model+ " " + car1.Type)
+//     },
 
-  //     Why: function () {
-  //       console.log("Its a" + "" +car1.Model + "A Beast in a Suit")
+//     Why: function () {
+//       console.log("Its a" + "" +car1.Model + "A Beast in a Suit")
 
-  //     },
-     
-  //     myJun: function () {if (car1.Km >= 100000){
-  //       console.log(`Your car is stil breathing no need for Mechanical check ups `);
-  //     }
-  //     else {
-  //               console.log(`Take your car for mechanical checks and repairs `);
+//     },
 
-  //     }
+//     myJun: function () {if (car1.Km >= 100000){
+//       console.log(`Your car is stil breathing no need for Mechanical check ups `);
+//     }
+//     else {
+//               console.log(`Take your car for mechanical checks and repairs `);
 
-  //     },
+//     }
 
-  //   },
+//     },
 
-  //   car2 = {
-  //     Model: "RS3",
-  //     Type : "Audi",
-  //     Year: 2023,
-  //     Km: 90000,
-  //     Origin: "Germany",
-  //     isHybraid: false,
-  //     mycar: function () {
-  //       return ("Hello your car is a"+ " "+ car2.Model+ " " +car2.Type)
-  //     },
+//   },
 
-  //     Why: function () {
-  //       console.log("Its a" + "" +car2.Model + "A Beast in a Suit")
+//   car2 = {
+//     Model: "RS3",
+//     Type : "Audi",
+//     Year: 2023,
+//     Km: 90000,
+//     Origin: "Germany",
+//     isHybraid: false,
+//     mycar: function () {
+//       return ("Hello your car is a"+ " "+ car2.Model+ " " +car2.Type)
+//     },
 
-  //     },
-     
-  //     myJun: function () {if (car2.Km >= 100000){
-  //       console.log(`Your car is stil breathing no need for Mechanical check ups `);
-  //     }
-  //     else {
-  //               console.log(`Take your car for mechanical checks and repairs `);
+//     Why: function () {
+//       console.log("Its a" + "" +car2.Model + "A Beast in a Suit")
 
-  //     }
+//     },
 
-  //     },
+//     myJun: function () {if (car2.Km >= 100000){
+//       console.log(`Your car is stil breathing no need for Mechanical check ups `);
+//     }
+//     else {
+//               console.log(`Take your car for mechanical checks and repairs `);
 
-  //   },
-  //   car3 = {
-  //     Model: "X5",
-  //     Type : "BMW",
-  //     Year: 2020,
-  //     Km: 110000,
-  //     Origin: "Germany",
-  //     isHybraid: true,
-  //     mycar: function () {
-  //       return ("Hello your car is a"+ " "+  car3.Model+ " " + car3.Type)
-  //     },
+//     }
 
-  //     Why: function () {
-  //       console.log("Its a" + "" + car3.Model + "A Beast in a Suit")
+//     },
 
-  //     },
-     
-  //     myJun: function () {if ( car3.Km >= 100000){
-  //       console.log(`Your car is stil breathing no need for Mechanical check ups `);
-  //     }
-  //     else {
-  //               console.log(`Take your car for mechanical checks and repairs `);
+//   },
+//   car3 = {
+//     Model: "X5",
+//     Type : "BMW",
+//     Year: 2020,
+//     Km: 110000,
+//     Origin: "Germany",
+//     isHybraid: true,
+//     mycar: function () {
+//       return ("Hello your car is a"+ " "+  car3.Model+ " " + car3.Type)
+//     },
 
-  //     }
+//     Why: function () {
+//       console.log("Its a" + "" + car3.Model + "A Beast in a Suit")
 
-  //     },
+//     },
 
-  //   },
-  //   car4 = {
-  //     Model: "GTI 7,5",
-  //     Type : "WV",
-  //     Year: 2023,
-  //     Km: 180000,
-  //     Origin: "Germany",
-  //     isHybraid: false,
-  //     mycar: function () {
-  //       return ("Hello your car is a"+ " "+  car4.Model+ " " + car4.Type)
-  //     },
+//     myJun: function () {if ( car3.Km >= 100000){
+//       console.log(`Your car is stil breathing no need for Mechanical check ups `);
+//     }
+//     else {
+//               console.log(`Take your car for mechanical checks and repairs `);
 
-  //     Why: function () {
-  //       console.log("Its a" + "" + car4.Model + "A Beast in a Suit")
+//     }
 
-  //     },
-     
-  //     myJun: function () {if ( car4.Km >= 100000){
-  //       console.log(`Your car is stil breathing no need for Mechanical check ups `);
-  //     }
-  //     else {
-  //               console.log(`Take your car for mechanical checks and repairs `);
+//     },
 
-  //     }
+//   },
+//   car4 = {
+//     Model: "GTI 7,5",
+//     Type : "WV",
+//     Year: 2023,
+//     Km: 180000,
+//     Origin: "Germany",
+//     isHybraid: false,
+//     mycar: function () {
+//       return ("Hello your car is a"+ " "+  car4.Model+ " " + car4.Type)
+//     },
 
-  //     },
+//     Why: function () {
+//       console.log("Its a" + "" + car4.Model + "A Beast in a Suit")
 
-  //   },
-     
-  // ];
+//     },
 
-  // console.log(car4.mycar())
-  // console.log(car4.Why())
-  // console.log(car4.myJun()
+//     myJun: function () {if ( car4.Km >= 100000){
+//       console.log(`Your car is stil breathing no need for Mechanical check ups `);
+//     }
+//     else {
+//               console.log(`Take your car for mechanical checks and repairs `);
+
+//     }
+
+//     },
+
+//   },
+
+// ];
+
+// console.log(car4.mycar())
+// console.log(car4.Why())
+// console.log(car4.myJun()
+
+// const names = ['Nathan','life','Phenyo','Jade']
+
+// for (let index = 0; index <names.length; index++) {
+//   console.log(names)
+
+// }
+
+//   const names = ['Nathan','life','Phenyo','Jade'];
+
+//  names.forEach(element => {
+//   console.log(names)
+
+//   });
+
+// for (let index = 0; index <6; index++) {
+//  if (index==3){
+//   continue
+//  }
+//   console.log(index)
+// }
+
+// let i =0;
+// while (i < 10){
+//   i++;
+//   console.log(i)
+// }
+
+// let i = 0;
+// do{
+//   i ++;
+//   console.log(i)
+// }
+
+// while (i < 5)
+const prompt = require("prompt-sync")();
+// let amountDeposited ;
+const deposit = () => {
+  // let amountDeposited = Number (prompt ('Please enter the amount you want to deposit'));
+
+  while (true) {
+    const amountDeposited = Number(
+      prompt("Please enter the amount you want to deposit: ")
+    );
   
+    const depositedAmount = Number(amountDeposited );
 
-  // const names = ['Nathan','life','Phenyo','Jade']
+    if (
+      depositedAmount <= 10 ||
+      depositedAmount == " " ||
+      depositedAmount == isNaN()
+    ) {
+      // break;
+      console.log("please enter valid number");
+    } else {
+      console.log(`R${depositedAmount} was deposited`);
+    }
+  }
+};
 
-  // for (let index = 0; index <names.length; index++) {
-  //   console.log(names)
-    
-  // }
-
-  const names = ['Nathan','life','Phenyo','Jade']
-
- names.forEach(element => {
-  console.log(names)
-
-  });
-
-
-
-
-
-
-
+const depositedAmount = deposit();
+console.log(`your amount deposited ${depositedAmount}`);
